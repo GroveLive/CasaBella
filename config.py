e72cc335306891a8b86f060bf2bf25b9b57c671d
GroveLive <andresjerezag@gmail.com>
2025-08-05 16:15:28 -0500

    holaxd

---
+++ config.py
@@ -0,0 +1,26 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
+    DATABASE_URL: str = ""
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    APP_PORT: int = 8001
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+    @property
+    def constructed_database_url(self):
+        if self.DATABASE_URL:
+            return self.DATABASE_URL
+        return f"postgresql+psycopg2://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
+
+settings = Settings()
